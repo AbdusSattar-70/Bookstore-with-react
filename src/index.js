b71fb9d4c2634a72ab4e58e3a1fb916d105d720c
Abdus Sattar <sasorganicagro@gmail.com>
2023-04-12 09:42:56 +0600

    Deploy with gh-pages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { HashRouter } from 'react-router-dom';
 
 import store from './redux/store';
 import './index.css';
@@ -10,7 +11,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <HashRouter>
+        <App />
+      </HashRouter>
     </Provider>
   </React.StrictMode>,
 );
